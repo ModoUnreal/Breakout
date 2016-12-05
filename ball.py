7b975591f607ef41ff18075cb014f335861de48a
BadassBanana <alejandrohurtado888@gmail.com>
2016-12-05 07:10:20 +0000

    forgot to add some stuff

---
+++ ball.py
@@ -0,0 +1,41 @@
+import pygame as pg
+import constants
+
+
+class Ball(pg.sprite.Sprite):
+    def __init__(self, player):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load(constants.BALL_DIR).convert()
+        self.image.set_colorkey(constants.WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (constants.WIDTH / 2, constants.HEIGHT / 2)
+        self.speedx = 3
+        self.speedy = 3
+        self.score = 0
+
+        self.player = player
+
+    def update(self):
+        self.check_walls()
+        self.check_bricks()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+    def check_walls(self):
+        if self.rect.x >= 1265 or self.rect.x <= 0:
+            self.speedx *= -1
+
+        if pg.sprite.collide_rect(self, self.player):
+            self.speedy *= -1
+
+        if self.rect.y < 0:
+            self.speedy *= -1
+
+        if self.rect.y > 700:
+            self.rect.x = 400
+            self.rect.y = 300
+
+    def check_bricks(self):
+        if pg.sprite.spritecollide(self, constants.brick_sprites, True, collided=None):
+            self.speedy *= -1
+            self.score += 1
